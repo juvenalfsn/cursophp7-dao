9f8a93771f9abcebad5f70634706eacb0b54ba57
juvenalfsn <juvenalfsn@outlook.com>
2021-04-19 17:50:00 -0300

    inserir e alterar

--- index.php
+++ index.php
@@ -15,9 +15,26 @@ require_once("config.php");
 //echo json_encode($search);
 
 //carrega um usuário usando oi login e senha
-$usuario = new Usuario();
-$usuario->login("caetano", "432987");
+//$usuario = new Usuario();
+//$usuario->login("caetano", "432987");
 
+//echo $usuario;
+/*
+//Criando um novo usuário 
+$aluno = new Usuario();
+
+$aluno->setDeslogin("domingas");
+$aluno->setDessenha("654789");
+
+$aluno->insert();
+echo $aluno;
+*/
+$usuario = new Usuario();
+
+$usuario->loadByID(7);
+
+$usuario->update("professor", "@#$0921");
 echo $usuario;
+
 
 ?>
