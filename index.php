5121e1616280ac065933ae954407f4f5cf0e4675
juvenalfsn <juvenalfsn@outlook.com>
2021-04-19 16:54:57 -0300

    meduto de lista

--- index.php
+++ index.php
@@ -1,9 +1,23 @@
 <?php
 require_once("config.php");
 
-$root = new Usuario();
+//Carrega um usuário
+//$root = new Usuario();
+//$root->loadByID(5);
+//echo $root;
 
-$root->loadByID(5);
-echo $root;
+//Carrega uma lista de usuários
+//$lista = Usuario ::getList();
+//echo json_encode($lista);
+
+//Carrega uma lista de usuários buscando por login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//carrega um usuário usando oi login e senha
+$usuario = new Usuario();
+$usuario->login("caetano", "432987");
+
+echo $usuario;
 
 ?>
